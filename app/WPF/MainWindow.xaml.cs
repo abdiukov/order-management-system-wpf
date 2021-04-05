a7c92d89fb224ca72a6a4f7f45a0c0396600aaf4
a.abdiukov <a.abdiukov@gmail.com>
2021-04-05 21:44:55 +1000

    update .gitignore, remove unnecessary features from LoginAuth, remove old project

--- app/WPF/MainWindow.xaml.cs
+++ app/WPF/MainWindow.xaml.cs
@@ -1,29 +1,15 @@
 ﻿using Auth0.OidcClient;
 using IdentityModel.OidcClient;
-using IdentityModel.OidcClient.Browser;
 using System.Collections.Generic;
 using System.Configuration;
-using System.Text;
 using System.Windows;
 using View;
 
 namespace WPFSample
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         private Auth0Client client;
-        readonly string[] _connectionNames = new string[]
-        {
-            "Username-Password-Authentication",
-            "google-oauth2",
-            "twitter",
-            "facebook",
-            "github",
-            "windowslive"
-        };
 
         public MainWindow()
         {
@@ -41,13 +27,10 @@ namespace WPFSample
                 ClientId = clientId
             });
 
-            var extraParameters = new Dictionary<string, string>();
-
-            if (!string.IsNullOrEmpty(connectionNameComboBox.Text))
-                extraParameters.Add("connection", connectionNameComboBox.Text);
-
-            if (!string.IsNullOrEmpty(audienceTextBox.Text))
-                extraParameters.Add("audience", audienceTextBox.Text);
+            var extraParameters = new Dictionary<string, string>
+            {
+                { "connection", "Username-Password-Authentication" }
+            };
 
             DisplayResult(await client.LoginAsync(extraParameters: extraParameters));
         }
@@ -57,58 +40,14 @@ namespace WPFSample
             // Display error
             if (loginResult.IsError)
             {
-                resultTextBox.Text = loginResult.Error;
                 return;
             }
-
             MainView view = new MainView();
             view.Show();
-
-            logoutButton.Visibility = Visibility.Visible;
-            loginButton.Visibility = Visibility.Collapsed;
-
-            // Display result
-            StringBuilder sb = new StringBuilder();
-
-            sb.AppendLine("Tokens");
-            sb.AppendLine("------");
-            sb.AppendLine($"id_token: {loginResult.IdentityToken}");
-            sb.AppendLine($"access_token: {loginResult.AccessToken}");
-            sb.AppendLine($"refresh_token: {loginResult.RefreshToken}");
-            sb.AppendLine();
-
-            sb.AppendLine("Claims");
-            sb.AppendLine("------");
-            foreach (var claim in loginResult.User.Claims)
-            {
-                sb.AppendLine($"{claim.Type}: {claim.Value}");
-            }
-
-            resultTextBox.Text = sb.ToString();
+            this.Close();
         }
 
-        private void Window_Loaded(object sender, RoutedEventArgs e)
-        {
-            connectionNameComboBox.ItemsSource = _connectionNames;
-        }
-
-        private async void LogoutButton_Click(object sender, RoutedEventArgs e)
-        {
-            BrowserResultType browserResult = await client.LogoutAsync();
-
-            if (browserResult != BrowserResultType.Success)
-            {
-                resultTextBox.Text = browserResult.ToString();
-                return;
-            }
 
 
-            logoutButton.Visibility = Visibility.Collapsed;
-            loginButton.Visibility = Visibility.Visible;
-
-            audienceTextBox.Text = "";
-            resultTextBox.Text = "";
-            connectionNameComboBox.ItemsSource = _connectionNames;
-        }
     }
 }
